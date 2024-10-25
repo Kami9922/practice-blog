93f35ceef5c3ba9f2f047c7e01bf479ecdfbe0be
Vitalya <vitalikt_22@mail.ru>
2024-10-25 10:25:21 +0400

    Starting a project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import styled from 'styled-components'
+
+const Div = styled.div`
+	text-align: center;
+`
+
+function App() {
+	return (
+		<Div className='App'>
+			<header className='App-header'>
+				<div className=''></div>
+			</header>
+		</Div>
+	)
+}
+
+export default App

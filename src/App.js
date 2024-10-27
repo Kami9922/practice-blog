1fd6d3124f13435a9081ab7dd5f276dc67b07f69
Vitalya <vitalikt_22@mail.ru>
2024-10-27 10:09:12 +0400

    Реализация основного компонента блога с роутингом

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import styled from 'styled-components'
 
-const Div = styled.div`
+const Content = styled.div`
+	padding: 120px 0;
+`
+const H2 = styled.h2`
 	text-align: center;
 `
+const Header = () => <div>Шапка</div>
+const Footer = () => <div>Футер</div>
 
 function App() {
 	return (
-		<Div className='App'>
-			<header className='App-header'>
-				<div className=''></div>
-			</header>
-		</Div>
+		<>
+			<Header />
+			<Content>
+				<H2>Контент Страницы</H2>
+				<Routes>
+					<Route
+						path='/'
+						element={<div>Главная страницы</div>}
+					/>
+					<Route
+						path='/login'
+						element={<div>Авторизация</div>}
+					/>
+					<Route
+						path='/register'
+						element={<div>Регистрация</div>}
+					/>
+					<Route
+						path='/users'
+						element={<div>Пользователи</div>}
+					/>
+					<Route
+						path='/post'
+						element={<div>Новая статья</div>}
+					/>
+					<Route
+						path='/post/:postId'
+						element={<div>Статья</div>}
+					/>
+					<Route
+						path='*'
+						element={<div>Ошибка</div>}
+					/>
+				</Routes>
+			</Content>
+			<Footer />
+		</>
 	)
 }
 
